faae61cabb625c20123d16da383306d6114305bd
DenysTretiak <DenysTretiak@users.noreply.github.com>
2022-09-05 09:55:50 +0000

    deploy: 546a5657bfa82e21c2bd1ca566899a7a1657ad96

---
+++ precache-manifest.6f077ceae17724d4f4b5d0d4e38fce0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5252c9ed99443eb372acce6053972d",
+    "url": "https://DenysTretiak.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "6f8e1eaabe906320a50f",
+    "url": "https://DenysTretiak.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "d9b2b0064100b1cbac1d",
+    "url": "https://DenysTretiak.github.io/task-management-frontend/static/js/2.c162acc6.chunk.js"
+  },
+  {
+    "revision": "6f8e1eaabe906320a50f",
+    "url": "https://DenysTretiak.github.io/task-management-frontend/static/js/main.c3fb3a10.chunk.js"
+  },
+  {
+    "revision": "bb944ad47f1fb6ca88dc",
+    "url": "https://DenysTretiak.github.io/task-management-frontend/static/js/runtime~main.a49934af.js"
+  }
+]);
